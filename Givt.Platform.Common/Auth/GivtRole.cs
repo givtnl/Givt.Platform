dd7de1578452d0503edd923b2b90d4ca7c14399c
Alex <alex@givtapp.net>
2022-07-06 21:00:33 +0200

    Moved Roles and Authorisation Middleware

---
+++ Givt.Platform.Common/Auth/GivtRole.cs
@@ -0,0 +1,7 @@
+﻿namespace Givt.Platform.Common.Auth;
+
+public enum GivtRole
+{
+    GivtAdmin = 0x0001,
+    GivtOperator = 0x0002
+}
---
+++ Givt.Platform.Common/Enums/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace Givt.Platform.Common.Enums;
+
+public enum Role
+{
+    // numbers are used to provide permission checks: if ((user.Role | requiredRole(s)) != 0) --> allow access
+    CampaignManager = 0x0001,
+    Treasurer = 0x0002,
+
+    Donor = 0x0100,
+}
